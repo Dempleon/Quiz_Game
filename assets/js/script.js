901da463c2323997327b4a6245fc8808d0242ef2
Dempoleon <dempoleon@gmail.com>
2022-10-02 19:53:11 -0700

    sample gage

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+var questionTxt = document.querySelector('#question')
+var answers = document.querySelectorAll('li');
+
+var question1 = {
+    question: 'This is just filler text for test',
+    answers: ['answer0', 'answer1', 'answer2', 'answer3'],
+}
