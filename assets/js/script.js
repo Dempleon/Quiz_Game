a49bcc957645f57b809f039155ea4f3ade000701
Dempoleon <dempoleon@gmail.com>
2022-10-02 20:43:59 -0700

    added answer highlight, event listener to question answers

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,52 @@
 var questionTxt = document.querySelector('#question')
 var answers = document.querySelectorAll('li');
 
-var question1 = {
-    question: 'This is just filler text for test',
-    answers: ['answer0', 'answer1', 'answer2', 'answer3'],
+var questions = [
+    {
+        question: 'This question 1 is just filler text for test',
+        answers: ['something', 'answer 1', 'answer 2', 'answer 3'],
+        correctAnswer: 'answer 1'
+    },
+    {
+        question: 'This question 2 is just filler text for test',
+        answers: ['answer 0', 'answer 1', 'answer 2', 'answer 3'],
+        correctAnswer: 1
+    },
+    {
+        question: 'This question 3 is just filler text for test',
+        answers: ['answer 0', 'answer 1', 'answer 2', 'answer 3'],
+        correctAnswer: 1
+    },
+    {
+        question: 'This question 4 is just filler text for test',
+        answers: ['answer 0', 'answer 1', 'answer 2', 'answer 3'],
+        correctAnswer: 1
+    },
+    {
+        question: 'This question 5 is just filler text for test',
+        answers: ['answer 0', 'answer 1', 'answer 2', 'answer 3'],
+        correctAnswer: 1
+    }
+]
+
+for (var i = 0; i < answers.length; i++) {
+    answers[i].addEventListener('click', clickQuestion);
 }
+
+function clickQuestion(event) {
+    console.log(event);
+    console.log(event.path[0]);
+    console.log(event.target.innerText);
+    if(event.target.innerText === questions[0].correctAnswer) {
+        console.log('correct');
+    }
+}
+
+function setQuestion(question) {
+    questionTxt.textContent = question["question"];
+    for (var i = 0; i < answers.length; i++) {
+        answers[i].textContent = question["answers"][i];
+    }
+}
+
+setQuestion(questions[0]);
